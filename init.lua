c49f00a09b6fe13b1fcf7a1eb63b9ea97e2e6281
Ryan Winchester <ryan@winchester.dev>
2024-03-17 18:39:33 -0300

    config things

--- init.lua
+++ init.lua
@@ -817,6 +817,17 @@ require('lazy').setup {
   },
 
   {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+  },
+
+  {
     'ThePrimeagen/harpoon',
     branch = 'harpoon2',
     dependencies = { 'nvim-lua/plenary.nvim' },
